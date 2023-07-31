2cace2cad7e220d8854a65ff5a64a3a1ed40fe63
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-07-31 16:58:07 +0300

    Update 2023-07-31T13:58:00.294Z

--- src/blocks/navigation/navigation.js
+++
@@ -1,6 +0,0 @@
-const navigationHamburger = document.querySelector('.navigation__hamburger');
-const navigationList = document.querySelector('.navigation__list');
-
-navigationHamburger.addEventListener('click', () => {
-  navigationList.classList.toggle('navigation__list_active');
-});
